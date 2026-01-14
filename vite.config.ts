e6e032f43dd3c916e3df083304152f9fa359a24a
a-rossman0825 <anthony.rossman0825@gmail.com>
2026-01-13 21:42:21 -0700

    chore: config vite lib build & public entry -2

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'ShoehornUI',
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'umd']
+    },
+
+    rollupOptions: {
+      // Vue must NOT be bundled
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
