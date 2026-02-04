3a84256f3a6d2062d503ad017ceec07a03d49497
a-rossman0825 <anthony.rossman0825@gmail.com>
2026-02-04 15:23:22 -0700

    fix: style.css export error

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,36 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
 
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'ShoehornUI',
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "ShoehornUI",
       fileName: (format) => `index.${format}.js`,
-      formats: ['es', 'umd']
+      formats: ["es", "umd"],
     },
+    cssCodeSplit: false,
     rollupOptions: {
       // Vue must NOT be bundled
-      external: ['vue'],
+      external: ["vue"],
       output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith('.css')) {
+            return "style.css";
+          }
+          return assetInfo.name || "";
+        },
         globals: {
-          vue: 'Vue'
+          vue: "Vue",
         },
       },
     },
   },
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
   },
-})
+});
