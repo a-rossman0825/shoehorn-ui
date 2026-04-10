d16cdc34249aba0949f1a8ef5bb615ef88c9fc94
a-rossman0825 <anthony.rossman0825@gmail.com>
2026-04-10 17:58:04 -0600

    test: ShButton tests for render & emits

---
+++ src/components/Button/ShButton.test.ts
@@ -0,0 +1,27 @@
+import { describe, it, expect } from 'vitest';
+import { mount } from "@vue/test-utils";
+import { ShButton } from './index';
+
+
+
+describe("ShButton", () => {
+  it ("renders as button on mount", ()=> {
+    const wrapper = mount(ShButton);
+    expect(wrapper.element.tagName).toBe("BUTTON");
+  });
+
+  it ("renders as anchor when props 'as: a' are passed", ()=> {
+    //NOTE - href necessary when passing "as: a" props (else dev warning fires)
+    const wrapper = mount(ShButton, { props: { as: "a"}, attrs: { href: "#" }});
+    expect(wrapper.element.tagName).toBe("A");
+  });
+
+  it ("emits 'click' when enabled and emit length === 1", async () => {
+    const wrapper = mount(ShButton);
+    const btn = wrapper.find("button");
+    await btn.trigger('click');
+    const event = wrapper.emitted("click");
+    if (!event) throw new Error('event does not exist/is falsy');
+    expect(event.length).toBe(1);
+  })
+});
